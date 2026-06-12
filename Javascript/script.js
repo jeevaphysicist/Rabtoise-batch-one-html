914b742ccdd5ad1ffffe73460a71698a20dee598
“jeevaphysicist” <jeevaphysicist@gmail.com>
2026-06-12 19:57:57 +0530

    Learn Tailwind css and Javascripts Basics

---
+++ Javascript/script.js
@@ -0,0 +1,41 @@
+console.log("Hello World");
+console.log("Welcome to Javascript");
+console.log("This is my first Javascript code");
+
+// Var , let and Const
+
+var name = "Jeevanantham";
+
+
+var name = "Rabtoise Technologeis"
+    name = "Nithiya"
+console.log(name);
+
+
+let name1 = "Jeeva 1 ";
+console.log(name1);
+
+    name1 = "Jeeva 2";
+
+console.log(name1);
+
+
+const name2 = "jeeva 3";
+console.log(name2); 
+
+//  String ,Number , Boolen, Undefined, Null, Symbol ,Array, Object
+
+// String Data Type
+let str1 = "Hello World";
+let str2 = 'Hello World single quote';
+let str3 = `Hello World backtick`;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+console.log(str1, str2, str3);
+console.log(str1 + " " + str2 + " " + str3);
+console.log(`${str1}  ${str2} ${str3}`);
+
+
